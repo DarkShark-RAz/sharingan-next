b66640cd76409d73be53139d0c07e29d47c7793d
Snehal Raj <snehalraj85@gmail.com>
2023-04-28 01:45:58 +0530

    feat: initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,8 +11,10 @@ module.exports = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-sharingan':
+          'radial-gradient(circle, rgba(0,0,0,1) 0%, rgba(110,12,12,1) 39%, rgba(75,8,8,1) 60%, rgba(0,0,0,1) 100%)',
       },
     },
   },
   plugins: [],
-}
+};
